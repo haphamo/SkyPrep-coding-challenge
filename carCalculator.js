74198e2a89f598191d773427d34d59b1d6631e81
haphamo <hapham0392@gmail.com>
2020-01-30 22:02:32 -0500

    created addOn data

--- carCalculator.js
+++ carCalculator.js
@@ -9,6 +9,40 @@ const fixture = [
 	}
 ]
 
+const basePrice = 24999
+const salesTax = 13/100
+const fixedAdminFees = 1200
+
+const addOns = [
+  {
+    addOn: "AWD Drivetrain",
+    price: 2500
+  },
+  {
+    addOn: "GPS Navigation",
+    price: 2000
+  },
+  {
+    addOn: "Winter Tire Package",
+    price: 2000
+  },
+  {
+    addOn: "Sport Package",
+    price: 3500
+  },
+  {
+    addOn: "Live Traffic Updates",
+    price: 1500
+  },
+  {
+    addOn: "Roadside Assistance",
+    price: 2500
+  }
+]
+ 
+
+             
+
 const carCalculator = function(data) {
   return `The cost for this car is X with the following configurations`
 }
